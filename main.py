29cbfa6b3c21dd56f137ba437879b17f905772a3
К-Эндрю-Ю <kozmaprudkov@gmail.com>
2024-09-27 15:53:43 +0300

    создал функцию деление без обработки исключений

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def subtruct (x,y):#subtruct - вычитание
     return x-y
 def multiply (a,b): #multiply - умножение
     return a*b
+def divide (x,y): # divide - деление
 
 print(subtruct (10,4))
 print(add (3,4))
 print(multiply (2,4))
+print(divide (10,2))
